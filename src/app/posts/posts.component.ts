7ca1076f73bb779b3b7043b5b69272d886f3dc73
FraLand <61711024+fraland@users.noreply.github.com>
2022-07-05 20:37:35 -0300

    intento de funciones de add, delete y edit

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestore, AngularFirestoreCollection  } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument  } from '@angular/fire/compat/firestore';
 import { Observable, tap } from 'rxjs';
 
 export interface Item { name: string; }
+export interface Post {
+  author: string,
+  content: string,
+  title: string
+
+}
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -12,18 +18,44 @@ export interface Item { name: string; }
 export class PostsComponent implements OnInit {
 
   posts: Observable<any[]>;
-  constructor(firestore: AngularFirestore){
-    this.posts = firestore.collection('posts').snapshotChanges().pipe(tap(data => console.log(data)));
+  constructor(private afs: AngularFirestore){
+    this.posts = afs.collection('posts').snapshotChanges().pipe(tap(data => console.log(data)));
 
-    this.itemsCollection = firestore.collection<Item>('items');
-    this.items = this.itemsCollection.valueChanges();
+    // Step 1: conectar la coleccion en firebase a una variable
+    this.PostManu = afs.doc<Post>('posts/quKWCoIlLrlEfS1D88G5')
+    this.itemsCollection = afs.collection<Item>('items');
+
+    // Step 2: la parte observable
+    this.PostManuObs = this.PostManu.valueChanges()
+    this.itemsObs = this.itemsCollection.valueChanges();
 
   }
+  itemsCollection: AngularFirestoreCollection<Post>;
+  itemsObs: Observable<Item[]>;
 
-  private itemsCollection: AngularFirestoreCollection<Item>;
-  items: Observable<Item[]>;
+  PostManu: AngularFirestoreDocument<Item>
+  PostManuObs: Observable<Post[]>
 
   ngOnInit(): void {
+
+
+
+
+
+
+  const doc = this.afs.doc('books/bookID');
+
+  const data = {
+    author: 'Jeff Delaney',
+    title: 'The Angular Firebase Survival Guide',
+    year: 2017
+   };
+
+   doc.set(data); // reset all properties with new data
+   doc.update({ publisher: 'LeanPub' }); // update individual properties
+   doc.delete(); // update individual properties
+
+
   }
 
 }
